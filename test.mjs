7e44682350064ac06b7aba96efd59c2785dc08e0
cmdsree <asreevisakh@gmail.com>
2025-06-22 18:40:21 -0700

    fix: set Puppeteer to run in headless mode

--- test.mjs
+++ test.mjs
@@ -48,7 +48,7 @@ async function runTest() {
   if (!viteUrl) throw new Error('Could not detect Vite preview server port');
   logger('Detected Vite preview server at:', viteUrl);
 
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   const timeout = 5000;
   page.setDefaultTimeout(timeout);
